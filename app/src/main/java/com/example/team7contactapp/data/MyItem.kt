0aef00c84bbd8dfc8101f4a64d6385d0751f78a0
joye-seo <104261048+joye-seo@users.noreply.github.com>
2023-09-06 18:10:27 +0900

    [Feat] ContactFragment - DetailActivitiy Parcelable 연결

--- app/src/main/java/com/example/team7contactapp/data/MyItem.kt
+++ app/src/main/java/com/example/team7contactapp/data/MyItem.kt
@@ -1,6 +1,32 @@
 package com.example.team7contactapp.data
 
+import android.os.Parcelable
+import com.example.team7contactapp.R
+import kotlinx.parcelize.Parcelize
 
+object User {
+    val dataList = mutableListOf<MyItem>(
+        MyItem(R.drawable.donghyun, "윤동현", R.drawable.img_bookmarkon, "", "", ""),
+        MyItem(R.drawable.jinjoo, "황진주", R.drawable.img_bookmarkoff, "", "", ""),
+        MyItem(R.drawable.profiles, "서수현", R.drawable.img_bookmarkon, "", "", ""),
+        MyItem(R.drawable.profiles, "이종민", R.drawable.img_bookmarkon, "", "", ""),
+        MyItem(R.drawable.profiles, "이성진", R.drawable.img_bookmarkoff, "", "", ""),
+        MyItem(R.drawable.profiles, "최다현", R.drawable.img_bookmarkon, "", "", ""),
+        MyItem(R.drawable.profiles, "정용현", R.drawable.img_bookmarkoff, "", "", ""),
+        MyItem(R.drawable.profiles, "김에디", R.drawable.img_bookmarkon, "", "", ""),
+        MyItem(R.drawable.profiles, "박에디", R.drawable.img_bookmarkon, "", "", ""),
+        MyItem(R.drawable.profiles, "심에디", R.drawable.img_bookmarkon, "", "", ""),
+        MyItem(R.drawable.profiles, "임에디", R.drawable.img_bookmarkon, "", "", ""),
+        MyItem(R.drawable.profiles, "정에디", R.drawable.img_bookmarkon, "", "", ""),
+        MyItem(R.drawable.profiles, "정에디", R.drawable.img_bookmarkon, "", "", ""),
+        MyItem(R.drawable.profiles, "정에디", R.drawable.img_bookmarkon, "", "", ""),
+        MyItem(R.drawable.profiles, "정에디", R.drawable.img_bookmarkon, "", "", ""),
+        MyItem(R.drawable.profiles, "정에디", R.drawable.img_bookmarkon, "", "", "")
+    )
+}
+
+
+@Parcelize
 data class MyItem(
     val icon: Int,
     val name: String,
@@ -8,4 +34,4 @@ data class MyItem(
     val address: String,
     val birth: String,
     val memo: String
-)
+) : Parcelable
--- app/src/main/java/com/example/team7contactapp/home/ContactFragment.kt
+++ app/src/main/java/com/example/team7contactapp/home/ContactFragment.kt
@@ -1,17 +1,15 @@
 package com.example.team7contactapp.home
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.team7contactapp.ContactDetailActivity
 import com.example.team7contactapp.ContactDialogFragment
 import com.example.team7contactapp.R
 import com.example.team7contactapp.adapter.ContactFragmentAdapter
-import com.example.team7contactapp.data.MyItem
+import com.example.team7contactapp.data.User.dataList
 import com.example.team7contactapp.databinding.FragmentContactBinding
 
 class ContactFragment : Fragment() {
@@ -35,24 +33,6 @@ class ContactFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         //여기서부터 작업
         //데이터 원본준비
-        val dataList = mutableListOf<MyItem>()
-        dataList.add(MyItem(R.drawable.donghyun, "윤동현", R.drawable.img_bookmarkon,"","",""))
-        dataList.add(MyItem(R.drawable.jinjoo, "황진주", R.drawable.img_bookmarkoff,"","",""))
-        dataList.add(MyItem(R.drawable.profiles, "서수현", R.drawable.img_bookmarkon,"","",""))
-        dataList.add(MyItem(R.drawable.profiles, "이종민", R.drawable.img_bookmarkon,"","",""))
-        dataList.add(MyItem(R.drawable.profiles, "이성진", R.drawable.img_bookmarkoff,"","",""))
-        dataList.add(MyItem(R.drawable.profiles, "최다현", R.drawable.img_bookmarkon,"","",""))
-        dataList.add(MyItem(R.drawable.profiles, "정용현", R.drawable.img_bookmarkoff,"","",""))
-        dataList.add(MyItem(R.drawable.profiles, "김에디", R.drawable.img_bookmarkon,"","",""))
-        dataList.add(MyItem(R.drawable.profiles, "박에디", R.drawable.img_bookmarkon,"","",""))
-        dataList.add(MyItem(R.drawable.profiles, "심에디", R.drawable.img_bookmarkon,"","",""))
-        dataList.add(MyItem(R.drawable.profiles, "임에디", R.drawable.img_bookmarkon,"","",""))
-        dataList.add(MyItem(R.drawable.profiles, "정에디", R.drawable.img_bookmarkon,"","",""))
-        dataList.add(MyItem(R.drawable.profiles, "정에디", R.drawable.img_bookmarkon,"","",""))
-        dataList.add(MyItem(R.drawable.profiles, "정에디", R.drawable.img_bookmarkon,"","",""))
-        dataList.add(MyItem(R.drawable.profiles, "정에디", R.drawable.img_bookmarkon,"","",""))
-        dataList.add(MyItem(R.drawable.profiles, "정에디", R.drawable.img_bookmarkon,"","",""))
-
 
         val adapter = ContactFragmentAdapter(dataList)
         binding.recyclerview.adapter = adapter
