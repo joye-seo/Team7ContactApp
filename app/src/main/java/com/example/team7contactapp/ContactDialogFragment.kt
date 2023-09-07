fd2c60b21c5915cbef396b7a5fae1d2357651bae
joye-seo <104261048+joye-seo@users.noreply.github.com>
2023-09-07 21:40:37 +0900

    [FEAT] : ⚙ ContactDialogFragment 버튼 visible 추가 및 생일 dialog 구현

--- app/src/main/java/com/example/team7contactapp/ContactDialogFragment.kt
+++ app/src/main/java/com/example/team7contactapp/ContactDialogFragment.kt
@@ -1,8 +1,7 @@
 package com.example.team7contactapp
 
+import android.app.DatePickerDialog
 import android.app.Dialog
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +9,7 @@ import android.view.ViewGroup
 import android.view.Window
 import androidx.fragment.app.DialogFragment
 import com.example.team7contactapp.databinding.FragmentContactDialogBinding
+import java.util.Calendar
 
 class ContactDialogFragment : DialogFragment() {
     private var _binding: FragmentContactDialogBinding? = null
@@ -17,9 +17,8 @@ class ContactDialogFragment : DialogFragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setStyle(STYLE_NORMAL,R.style.DialogStyle)
+        setStyle(STYLE_NORMAL, R.style.DialogStyle)
     }
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -34,6 +33,51 @@ class ContactDialogFragment : DialogFragment() {
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
         return dialog
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.edtBirth.isFocusable = false
+        initView()
+    }
+
+    private fun initView() = with(binding) {
+        btnCancel.setOnClickListener {
+            dismiss()
+        }
+        btnSave.setOnClickListener {
+
+        }
+        btnMore.setOnClickListener {
+            layoutMore.visibility = View.VISIBLE
+            btnMore.visibility = View.GONE
+        }
+        edtBirth.setOnClickListener {
+            showDatePicker()
+        }
+    }
+
+    private fun showDatePicker() {
+        // 현재 날짜로 DatePickerDialog 초기화
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        // DatePickerDialog 생성 및 표시
+        val datePickerDialog = DatePickerDialog(
+            requireContext(),
+            DatePickerDialog.OnDateSetListener { view, selectedYear, selectedMonth, selectedDay ->
+                val selectedDate = "$selectedYear/${selectedMonth + 1}/$selectedDay"
+                binding.edtBirth.setText(selectedDate)
+            },
+            year,
+            month,
+            day
+        )
+
+        datePickerDialog.show()
+    }
+
 
     override fun onDestroy() {
         super.onDestroy()
