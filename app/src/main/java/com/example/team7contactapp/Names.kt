20fb7fd049d9d82ee8bfb61f3a8315f3f0e83b12
asdsad86642 <asdsad86642@gmail.com>
2023-09-08 16:40:09 +0900

    [Enhancement] 키패드 숫자 입력시 이미지 출력 & 비어 있을시 이미지 출력 하지 않음

--- app/src/main/java/com/example/team7contactapp/Names.kt
+++ app/src/main/java/com/example/team7contactapp/Names.kt
@@ -1,13 +1,12 @@
 package com.example.team7contactapp
 
- class Names (val call : Int ,
+import java.time.LocalDate
+
+class Names (val call : Int ,
               val name : String ,
               val time : String,
               val phoneNumber:String,
               val recode:String,
-              val phone : Int,
-              val message : Int,
-              val video : Int,
-              val information: Int,
               var isExpanded: Boolean = false
+
  )
