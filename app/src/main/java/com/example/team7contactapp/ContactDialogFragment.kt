4e9cbd9534d276d6ca2b68364ac918ef0a8a5a6a
joye-seo <104261048+joye-seo@users.noreply.github.com>
2023-09-09 22:01:24 +0900

    [FEAT] : ⚙️ 연락처 추가 페이지 에러메세지 버튼 이벤트 추가

--- app/src/main/java/com/example/team7contactapp/ContactDialogFragment.kt
+++ app/src/main/java/com/example/team7contactapp/ContactDialogFragment.kt
@@ -2,21 +2,29 @@ package com.example.team7contactapp
 
 import android.app.DatePickerDialog
 import android.app.Dialog
+import android.net.Uri
 import android.os.Bundle
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.Window
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.DialogFragment
 import com.example.team7contactapp.data.MyItem
 import com.example.team7contactapp.databinding.FragmentContactDialogBinding
 import java.util.Calendar
+import java.util.regex.Pattern
 
 class ContactDialogFragment : DialogFragment() {
     private var _binding: FragmentContactDialogBinding? = null
     private val binding get() = _binding!!
 
     private var listener: AddItem? = null
+
+    private var nameCorrect = false
+    private var phoneCorrect = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,7 +56,14 @@ class ContactDialogFragment : DialogFragment() {
             dismiss()
         }
         btnSave.setOnClickListener {
-            userData()
+            val name = binding.edtName.text.toString()
+            val phoneNumber = binding.edtNumber.text.toString()
+
+            if (name.isEmpty() || phoneNumber.isEmpty() || !nameCorrect || !phoneCorrect) {
+                Toast.makeText(requireContext(), "필수 값을 확인해주세요", Toast.LENGTH_SHORT).show()
+            } else {
+                userData()
+            }
         }
         btnMore.setOnClickListener {
             layoutMore.visibility = View.VISIBLE
@@ -56,6 +71,41 @@ class ContactDialogFragment : DialogFragment() {
         }
         edtBirth.setOnClickListener {
             showDatePicker()
+        }
+        textChanged()
+    }
+
+    private fun textChanged() = with(binding) {
+        val ps: Pattern =
+            Pattern.compile("^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$")
+
+        edtName.addTextChangedListener {
+            if (it.isNullOrBlank()) {
+                textInputLayoutName.error = "이름을 입력해주세요"
+                nameCorrect = false
+            } else {
+                textInputLayoutName.error = null
+                nameCorrect = true
+            }
+        }
+        edtNumber.addTextChangedListener {
+            if (it.isNullOrBlank()) {
+                textInputLayoutNumber.error = "연락처를 입력해주세요"
+                phoneCorrect = false
+            } else if (!it.toString().matches("[0-9]+".toRegex())) {
+                textInputLayoutNumber.error = "숫자만 입력해주세요"
+                phoneCorrect = false
+            } else {
+                textInputLayoutNumber.error = null
+                phoneCorrect = true
+            }
+        }
+        edtEmail.addTextChangedListener {
+            if (!ps.matcher(binding.edtEmail.text.toString()).matches()) {
+                textInputLayoutEmail.error = "이메일 형식을 확인해주세요"
+            } else {
+                textInputLayoutEmail.error = null
+            }
         }
     }
 
